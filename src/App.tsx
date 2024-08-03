4f4528517d0f5e00b4d1e85b3570e89f07835882
savarniknandinee <80510308+savarniknandinee@users.noreply.github.com>
2024-08-03 21:00:27 +0530

    Contact

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
 import { Route, Routes } from "react-router"
 import HomePage from "./pages/HomePage"
-import ThreeDWorld from "./pages/ThreeDWorld"
 import ContactPage from "./pages/ContactPage"
 import ProjectsPage from "./pages/ProjectsPage"
-import Game from "./pages/Game"
 import Page404 from "./pages/Page404"
 import Stats from "./pages/Stats"
 
@@ -35,9 +33,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/projects" element={<ProjectsPage />} />
-        <Route path="/3D" element={<ThreeDWorld />} />
         <Route path="/contact" element={<ContactPage />} />
-        <Route path="/game/*" element={<Game />} />
         <Route path="/stats" element={<Stats />} />
         <Route path="/*" element={<Page404 />} />
       </Routes>
